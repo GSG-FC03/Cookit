38a9b62957df3aef4ce6686971fa855815036d63
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2021-08-10 19:45:31 +0200

    write code js for cuisine page Relates #23

--- cousins/cousins.js
+++ cousins/cousins.js
@@ -1,17 +1,43 @@
-fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
-.then((response) => response.json())
-    .then(data => {
-        const meals= data.meals;
-        // console.log(meals);
+const mealContainer=document.getElementById("meal")
+let country = localStorage.getItem(0);
+console.log(country)
+fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`)
+.then(data => (data.json()))
+.then(Response => {
+    const mealsArray = Response.meals;
+    console.log(mealsArray)
+    for (i=0;i<=mealsArray.length;i++){
+    
+   
+    const mealBox=document.createElement("div")
+    mealBox.setAttribute ("class","meal-box")
+    mealContainer.appendChild(mealBox)
+    const imgContainer=document.createElement("div")
+   imgContainer.setAttribute ("class","meal-img")
+   mealBox .appendChild(imgContainer)
+   const mealimage=document.createElement("img")
+   mealimage.src =mealsArray[i].strMealThumb
+   mealBox.appendChild(mealimage)  
+   const mealName=document.createElement("div")
+    mealName.setAttribute ("class","meal-name")
+    mealBox.appendChild(mealName)
+    const mealHeader=document.createElement("h2")
+    mealHeader.textContent= mealsArray[i].strMeal
+    mealBox.appendChild(mealHeader)
+    const mealbutton=document.createElement("button")
+    mealbutton.setAttribute("class","recipe-button")
+    mealbutton.textContent= "Get Recipe"
+    mealbutton.addEventListener('click',()=>{
+        localStorage.setItem ("id",`${mealsArray[i].idMeal}`)
+        location.href="../recipe/recipe.html";
+    }
+    
+    )
+    
+    mealBox.appendChild(mealbutton)
+    
+  
+    }
+    
 
-        for (let i=0; i<=meals.length; i++){
-            console.log(meals[i].strArea);
-            if(meals[i].strArea){
-            let opt = document.createElement('option');
-            opt.value = meals[i].strArea;
-            opt.textContent =meals[i].strArea;
-
-            selectArea.appendChild(opt);
-            }
-        }
-        })
+})
