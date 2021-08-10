040f6ba4323326f3cc77984ba8ff488aca0b3328
Rola-H <rolahararah@gmail.com>
2021-08-10 14:31:20 +0300

    reconstructing home page JS file >Relates #25

--- Main/main.js
+++ Main/main.js
@@ -2,14 +2,14 @@
 const countryDiv = document.querySelector("#country-div");
 fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
   .then((response) => response.json())
-  .then((meals) => showMeals(meals.meals));
+  .then((meals) => showCountries(meals.meals));
 
-showMeals = (meals) => {
+showCountries = (meals) => {
   const mealsDiv = document.querySelector("#meal-div");
 
   meals.forEach((meals) => {
-    //to take the info we want from the api
 
+    //to take the info we want from the api
     const countrycuisine = document.createElement("div"); //create div in the html file
     mealsDiv.appendChild(countrycuisine); //make the div a child of the div in the html file
 
@@ -24,13 +24,14 @@ showMeals = (meals) => {
     recipesBtn.innerText = "Get Recipes"; //assign text to it
     recipesBtn.id = "GetRecipesBtn"; //assign an id to it
     countrycuisine.appendChild(recipesBtn); //make the button a child of the div
-  });
+
+        recipesBtn.addEventListener("click", function (e) {
+        console.log(recipesBtn.previousElementSibling.innerText);
+        localStorage.setItem("0",recipesBtn.previousElementSibling.innerText);
+        location.href ="../recipe/recipe.html";
+
+      });
+    });
 };
-// var colors = ["#0260E8", "#51EAFF", "#F5E027", "#FE9E76", "#DCABAE"];
-// var random_color = colors[Math.floor(Math.random() * colors.length)];
-// document.getElementById("GetRecipesBtn").style.backgroundColor = random_color;
-
-// ("#GetRecipesBtn").css("background-color", random_color);
-
 
 //END of Main page first meals display
