74df8bef18716c7f21965425b0519655bd53c0f6
Rola-H <rolahararah@gmail.com>
2021-08-12 15:12:24 +0300

    JS file for cuisine page >Relates #31

--- cousins/cousins.js
+++ cousins/cousins.js
@@ -1,108 +1,71 @@
 const mealContainer = document.getElementById("meal");
-const searchContainer = document.getElementById("search-meal");
 const errorContainer = document.getElementById("error-message");
+let searchContainer = document.getElementById("search-meal");
 let country = localStorage.getItem(0);
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
 let mealsArray = [];
+
 fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`)
   .then((data) => data.json())
   .then((Response) => {
     mealsArray = Response.meals;
-
-    for (i = 0; i <= mealsArray.length - 1; i++) {
-      // create div
-      const mealBox = document.createElement("div");
-      mealBox.setAttribute("class", "meal-box");
-      mealContainer.appendChild(mealBox);
-
-      // create imgContainer in the div
-      const imgContainer = document.createElement("div");
-      imgContainer.setAttribute("class", "meal-img");
-      mealBox.appendChild(imgContainer);
-
-      // create img element in the div
-      const mealimage = document.createElement("img");
-      mealimage.src = mealsArray[i].strMealThumb;
-      mealBox.appendChild(mealimage);
-
-      // create div in the div
-      const mealName = document.createElement("div");
-      mealName.setAttribute("class", "meal-name");
-      mealBox.appendChild(mealName);
-
-      // create h2 in the div
-      const mealHeader = document.createElement("h2");
-      mealHeader.textContent = mealsArray[i].strMeal;
-      mealBox.appendChild(mealHeader);
-
-      // create button in the div
-      const mealbutton = document.createElement("button");
-      mealbutton.setAttribute("class", "recipe-button");
-      mealbutton.textContent = "Get Recipe";
-      mealbutton.addEventListener("click", function (e) {
-        localStorage.setItem("1", mealbutton.previousElementSibling.value);
-        location.href = "../recipe/recipe.html";
-      });
-
-      mealBox.appendChild(mealbutton);
-    }
+    renderMeals(mealsArray, mealContainer);
+    return mealsArray;
   })
-  .then((response) => {
+  .then((meals) => {
     searchBtn.addEventListener("click", function (e) {
+      removeChildren(searchContainer);
+      mealContainer.style.display = "none";
       const inputValue = searchInput.value;
-      for (let i = 0; i < mealsArray.length; i++) {
-
-        if (mealsArray[i].strMeal.includes(inputValue)) {
-
-          console.log(mealsArray[i].strMeal);
-          mealContainer.style.display = "none"; //make meal container invisible
-          
-          // create div
-          const mealBox = document.createElement("div");
-          mealBox.setAttribute("class", "meal-box");
-          searchContainer.appendChild(mealBox);
-
-          // create imgContainer in the div
-          const imgContainer = document.createElement("div");
-          imgContainer.setAttribute("class", "meal-img");
-          searchContainer.appendChild(imgContainer);
-
-          // create img element in the div
-          const mealimage = document.createElement("img");
-          mealimage.src = mealsArray[i].strMealThumb;
-          searchContainer.appendChild(mealimage);
-
-          // create div in the div
-          const mealName = document.createElement("div");
-          mealName.setAttribute("class", "meal-name");
-          searchContainer.appendChild(mealName);
-
-          // create h2 in the div
-          const mealHeader = document.createElement("h2");
-          mealHeader.textContent = mealsArray[i].strMeal;
-          searchContainer.appendChild(mealHeader);
-
-          // create button in the div
-          const mealbutton = document.createElement("button");
-          mealbutton.setAttribute("class", "recipe-button");
-          mealbutton.textContent = "Get Recipe";
-          mealbutton.addEventListener("click", function (e) {
-            localStorage.setItem("1", mealbutton.previousElementSibling.value);
-            location.href = "../recipe/recipe.html";
-          });
-          searchContainer.appendChild(mealbutton);
-        }
-
-        else{
-          mealContainer.style.display = "none";
-          // searchContainer.style.display = "none";
-          // const errorMessage = document.createElement("h2");
-          // errorMessage.textContent = "no results with your input";
-          // console.log(errorMessage)
-          // errorContainer.appendChild(errorMessage);
-
-        }
-      }
+      const dataFilter = meals.filter((meal) =>
+        meal.strMeal.toUpperCase().includes(inputValue.toUpperCase())
+      );
+      renderMeals(dataFilter, searchContainer);
+      searchInput.value = "";
     });
   });
+
+function renderMeals(array, container) {
+  for (i = 0; i <= array.length - 1; i++) {
+    // create div
+    const mealBox = document.createElement("div");
+    mealBox.setAttribute("class", "meal-box");
+    container.appendChild(mealBox);
+
+    // create imgContainer in the div
+    const imgContainer = document.createElement("div");
+    imgContainer.setAttribute("class", "meal-img");
+    mealBox.appendChild(imgContainer);
+
+    // create img element in the div
+    const mealimage = document.createElement("img");
+    mealimage.src = array[i].strMealThumb;
+    mealBox.appendChild(mealimage);
+
+    // create div in the div
+    const mealName = document.createElement("div");
+    mealName.setAttribute("class", "meal-name");
+    mealBox.appendChild(mealName);
+
+    // create h2 in the div
+    const mealHeader = document.createElement("h2");
+    mealHeader.textContent = array[i].strMeal;
+    mealBox.appendChild(mealHeader);
+
+    // create button in the div
+    const mealbutton = document.createElement("button");
+    mealbutton.setAttribute("class", "recipe-button");
+    mealbutton.textContent = "Get Recipe";
+    mealbutton.addEventListener("click", function (e) {
+      localStorage.setItem("1", mealbutton.previousElementSibling.value);
+      location.href = "../recipe/recipe.html";
+    });
+
+    mealBox.appendChild(mealbutton);
+  }
+}
+
+function removeChildren(container) {
+  [...container.children].forEach((child) => child.remove());
+}
